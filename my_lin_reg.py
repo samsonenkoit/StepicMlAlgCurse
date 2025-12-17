3b9a4e35eecdb37b4e326977fd2e0efaa158923e
Michael Samsonenko <samsonenkoit@hotmail.com>
2025-12-17 17:16:54 +0600

    +

--- my_lin_reg.py
+++ my_lin_reg.py
@@ -1,7 +1,13 @@
+import pandas
+
+
 class MyLineReg():
-    def __init__(self, n_iter, learning_rate):
+    def __init__(self, n_iter, learning_rate, weights: pandas.DataFrame):
         self.n_inter = n_iter
         self.learning_rate = learning_rate
+        self.weights = weights
 
     def __str__(self):
         return f"MyLineReg class: n_iter={self.n_inter}, learning_rate={self.learning_rate}"
+
+    def fit()
