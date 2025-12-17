d48a007fc2903329ffbf6eb821a0915b751405fc
Michael Samsonenko <samsonenkoit@hotmail.com>
2025-12-17 16:36:41 +0600

    +

---
+++ my_lin_reg.py
@@ -0,0 +1,7 @@
+class MyLineReg():
+    def __init__(self, n_iter, learning_rate):
+        self.n_inter = n_iter
+        self.learning_rate = learning_rate
+
+    def __str__(self):
+        return f"MyLineReg class: n_iter={self.n_inter}, learning_rate={self.learning_rate}"
