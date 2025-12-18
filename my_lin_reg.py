e39af78732970e615b5df5965e2099bc6a30bab7
Michael Samsonenko <samsonenkoit@hotmail.com>
2025-12-18 17:00:55 +0600

    +

--- my_lin_reg.py
+++ my_lin_reg.py
@@ -1,4 +1,6 @@
 import pandas
+import numpy
+from typing import Union, Literal
 
 
 class MyLineReg():
@@ -10,4 +12,29 @@ class MyLineReg():
     def __str__(self):
         return f"MyLineReg class: n_iter={self.n_inter}, learning_rate={self.learning_rate}"
 
-    def fit()
+    def fit(self, X: pandas.DataFrame, y: pandas.Series, verbose: int | bool):
+        X.insert(0, 'x0', 1)
+
+        weights = pandas.Series(numpy.ones(X.shape[1]))
+
+        features_count = X.shape[0]
+
+        if (verbose > 0):
+            print(f'start | ${self._loss_function(y, X.dot(weights))}')
+
+        for step in range(self.n_inter):
+            predicted_values = X.dot(weights)
+            mseGrad = (predicted_values - y).dot(X).mul(2).div(features_count)
+
+            weights += mseGrad.mul(-1).mul(self.learning_rate)
+            self.weights = weights
+
+            if (verbose > 0 and step % verbose == 0):
+                print(f'start | ${self._loss_function(y, X.dot(weights))}')
+
+    def get_coef(self):
+        return self.weights.values[1:]
+
+    def _loss_function(self, y: pandas.Series, predicted: pandas.Series):
+        mse = sum(((predicted - y) ** 2).div(y.shape[0]))
+        return mse
