935f06b26d9821a380fa3d7441f123c73e5e3fcb
Michael Samsonenko <samsonenkoit@hotmail.com>
2025-12-22 17:03:12 +0600

    +

--- my_lin_reg.py
+++ my_lin_reg.py
@@ -3,10 +3,12 @@ import pandas as pd
 
 
 class MyLineReg():
-    def __init__(self, n_iter, learning_rate, weights: pd.Series = None):  # type: ignore
+
+    def __init__(self, n_iter, learning_rate, weights: pd.Series = None, metric: str = None):  # type: ignore
         self.n_inter = n_iter
         self.learning_rate = learning_rate
         self.weights = weights
+        self.metric = metric
 
     def __str__(self):
         return f"MyLineReg class: n_iter={self.n_inter}, learning_rate={self.learning_rate}"
@@ -19,7 +21,8 @@ class MyLineReg():
         features_count = X.shape[0]
 
         if (verbose > 0):
-            print(f'start | ${self._mse_loss_function(y, X.dot(weights))}')
+            self._print_education_score(
+                y, X, weights, verbose, 'start', self.metric)
 
         for step in range(self.n_inter):
             predicted_values = X.dot(weights)
@@ -29,16 +32,66 @@ class MyLineReg():
             self.weights = weights
 
             if (verbose > 0 and step % verbose == 0):
-                print(f'start | ${self._mse_loss_function(y, X.dot(weights))}')
+                self._print_education_score(
+                    y, X, weights, verbose, str(step), self.metric)
+
+            self._best_score = MyLineReg._calculate_metric(
+                self.metric, y, X.dot(self.weights))
 
     def predict(self, X: pd.DataFrame) -> float:
         X.insert(0, 'x0', 1)
         predicted_values = X.dot(self.weights)
         return sum(predicted_values)
 
+    def get_best_score(self) -> float:
+        return self._best_score
+
     def get_coef(self):
         return self.weights.values[1:]
 
-    def _mse_loss_function(self, y: pd.Series, predicted: pd.Series):
+    def _print_education_score(self, y: pd.Series,  X: pd.DataFrame, wg: pd.Series, verbose: bool, step: str = 'start', metric: str = None):  # type: ignore
+        if not verbose:
+            return
+
+        predicted = X.dot(wg)
+        prnt_str = f'{step}|loss {MyLineReg._mse(y, predicted)}'
+
+        if metric:
+            prnt_str += f'|{metric}: {MyLineReg._calculate_metric(metric, y, predicted)}'
+
+        print(prnt_str)
+
+    @staticmethod
+    def _calculate_metric(metric: str, y: pd.Series, predicted: pd.Series) -> float:
+        _metrics = {
+            'mae': MyLineReg._mae,
+            'mse': MyLineReg._mse,
+            'rmse': MyLineReg._rmse,
+            'mape': MyLineReg._mape,
+            'r2': MyLineReg._r2
+        }
+        return _metrics[metric](y, predicted)
+
+    @staticmethod
+    def _rmse(y: pd.Series, predicted: pd.Series) -> float:
+        return MyLineReg._mse(y, predicted) ** 0.5
+
+    @staticmethod
+    def _mae(y: pd.Series, predicted: pd.Series) -> float:
+        return sum(abs(predicted - y)) / y.shape[0]
+
+    @staticmethod
+    def _mse(y: pd.Series, predicted: pd.Series) -> float:
         val = sum(((predicted - y) ** 2).div(y.shape[0]))
         return val
+
+    @staticmethod
+    def _r2(y: pd.Series, predicted: pd.Series) -> float:
+        numerator = sum(((predicted - y) ** 2))
+        average_y = sum(y) / len(y)
+        denominator = sum((y - average_y) ** 2)
+        return 1 - (numerator / denominator)
+
+    @staticmethod
+    def _mape(y: pd.Series, predicted: pd.Series) -> float:
+        return sum(abs(y - predicted) / y) * (100/len(y))
