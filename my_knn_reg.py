33ca20ca134352dce4c0fe384a3cc9b477d8e537
Michael Samsonenko <samsonenkoit@hotmail.com>
2026-01-23 16:43:19 +0600

    +

--- my_knn_reg.py
+++ my_knn_reg.py
@@ -1,6 +1,17 @@
+import random
+import pandas as pd
+import numpy as np
+
+
 class MyKNNReg():
-    def __init__(self, k=3) -> None:
+    def __init__(self, train_size=None, k=3) -> None:
         self.k = k
+        self.train_size = train_size
 
     def __str__(self) -> str:
         return f'MyKNNReg class: k={self.k}'
+
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+        self.X = X
+        self.y = y
+        self.train_size = (X.shape[0], X.shape[1])
