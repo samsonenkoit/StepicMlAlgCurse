efbd2238578dbe162569c15a1fdf23e740a3cf33
msamsonenko <msamsonenko@hotmail.com>
2026-03-18 01:02:50 +0600

    +

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ y = y - 1
 
 items, thresholds = numpy.histogram(X[X.columns[0]], 3)
 
-tree = MyTreeClf(max_depth=5, min_samples_split=200, max_leafs=10, bins=4)
+tree = MyTreeClf(max_depth=4, min_samples_split=100, max_leafs=17, bins=16)
 tree.fit(X, y)
 tree.print_tree()
 print(tree.leafs_sum)
